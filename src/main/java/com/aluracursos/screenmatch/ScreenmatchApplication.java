40c4b0f67ecc69250b5ff8cd92c303b143d072fa
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-05-20 05:28:58 -0300

    Consumo de API

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo desde Spring");
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+
+		System.out.println(json);
 	}
 }
